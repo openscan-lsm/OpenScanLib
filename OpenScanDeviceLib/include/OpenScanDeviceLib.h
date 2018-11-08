d5658307314ddb8ac1ebec5dc7c4b54f64eae936
Mark A. Tsuchida <mark@open-imaging.com>
2018-11-08 10:37:20 -0800

    size_t requires stdlib.h

--- OpenScanDeviceLib/include/OpenScanDeviceLib.h
+++ OpenScanDeviceLib/include/OpenScanDeviceLib.h
@@ -8,6 +8,7 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 
 #ifdef __cplusplus
--- OpenScanLib/include/OpenScanLib.h
+++ OpenScanLib/include/OpenScanLib.h
@@ -6,6 +6,7 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #ifdef __cplusplus
 extern "C" {
