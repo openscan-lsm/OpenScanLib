d46608f54dea1a15a5a457c6c7d69774515942f0
zhangxiaosa <15927119728@163.com>
2021-02-20 14:30:24 -0600

    two lines between functions

--- OpenScanLib/src/Error.c
+++ OpenScanLib/src/Error.c
@@ -7,28 +7,38 @@ static RERR_ErrorMapPtr map;
 
 
 // APIs for device modules
-OSc_RichError *OScInternal_Error_RegisterCodeDomain(const char *domainName, RERR_CodeFormat codeFormat) {
+OSc_RichError *OScInternal_Error_RegisterCodeDomain(const char *domainName, RERR_CodeFormat codeFormat) 
+{
 	return RERR_Domain_Register(domainName, codeFormat);
 }
 
-OScDev_Error OScInternal_Error_ReturnAsCode(OScDev_RichError *error) {
+OScDev_Error OScInternal_Error_ReturnAsCode(OScDev_RichError *error) 
+{
 	return RERR_ErrorMap_RegisterThreadLocal(map, error);
 }
 
+
 // APIs for OpenScanLib
-OSc_RichError *OScInternal_Error_RetrieveRichErrors(int32_t code) {
+OSc_RichError *OScInternal_Error_RetrieveRichErrors(int32_t code) 
+{
 	return RERR_ErrorMap_RetrieveThreadLocal(map, code);
 }
 
-OSc_RichError *OScInternal_Error_Wrap(OSc_RichError *cause, const char *domainName, int32_t code, const char *message) {
+
+OSc_RichError *OScInternal_Error_Wrap(OSc_RichError *cause, const char *domainName, int32_t code, const char *message) 
+{
 	return RERR_Error_WrapWithCode(cause, domainName, code, message);
 }
 
-OSc_RichError *OScInternal_Error_Create(const char *domainName, int32_t code, const char *message) {
+
+OSc_RichError *OScInternal_Error_Create(const char *domainName, int32_t code, const char *message) 
+{
 	return RERR_Error_CreateWithCode(domainName, code, message);
 }
 
-char *OScInternal_Error_OScDomain() {
+
+char *OScInternal_Error_OScDomain() 
+{
 	static char *domainName = NULL;
 	if (domainName == NULL) {
 		domainName = "OpenScan";
@@ -37,7 +47,9 @@ char *OScInternal_Error_OScDomain() {
 	return domainName;
 }
 
-char *OScInternal_Error_LegacyCodeDomain() {
+
+char *OScInternal_Error_LegacyCodeDomain() 
+{
 	static char *domainName = NULL;
 	if (domainName == NULL) {
 		domainName = "LegacyDeviceErrorDomain";
@@ -46,22 +58,32 @@ char *OScInternal_Error_LegacyCodeDomain() {
 	return domainName;
 }
 
-const char *OSc_Error_GetMessage(OSc_RichError *error) {
+
+const char *OSc_Error_GetMessage(OSc_RichError *error) 
+{
 	return RERR_Error_GetMessage(error);
 }
 
-const char *OSc_Error_GetDomain(OSc_RichError *error) {
+
+const char *OSc_Error_GetDomain(OSc_RichError *error) 
+{
 	return RERR_Error_GetDomain(error);
 }
 
-int32_t OSc_Error_GetCode(OSc_RichError *error) {
+
+int32_t OSc_Error_GetCode(OSc_RichError *error) 
+{
 	return RERR_Error_GetCode(error);
 }
 
-OSc_RichError *OSc_Error_GetCause(OSc_RichError *error) {
+
+OSc_RichError *OSc_Error_GetCause(OSc_RichError *error) 
+{
 	return RERR_Error_GetCause(error);
 }
 
-void OSc_Error_Destroy(OSc_RichError *error) {
+
+void OSc_Error_Destroy(OSc_RichError *error) 
+{
 	RERR_Error_Destroy(error);
 }
