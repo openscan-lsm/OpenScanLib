b2c44538d296df8dc4f040eaaf9b8a4cd2f00731
zhangxiaosa <15927119728@163.com>
2021-03-09 23:09:58 -0600

    delete irrelevent files

--- TestDeviceModule/DeviceModules.h
+++
@@ -1,7 +0,0 @@
-#include "../OpenScanDeviceLib/include/OpenScanDeviceLib.h"
-#include "../OpenScanLib/include/OpenScanLib.h"
-
-
- OScDev_DeviceImpl g_TestDeviceImpl2;
-
- void testDeviceMethods(OSc_Device *device);
