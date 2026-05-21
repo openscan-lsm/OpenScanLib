5c712cf11d00887859317adecd40d738fac3072a
Gabriel Selzer <gjselzer@wisc.edu>
2026-05-21 09:31:12 -0500

    Format

--- OpenScanLib/src/Module.c
+++ OpenScanLib/src/Module.c
@@ -72,11 +72,9 @@ error:
 
 OSc_RichError *OScInternal_Module_Load(OScInternal_Module *module,
                                        const char *path) {
-    *module = LoadLibraryExA(
-        path,
-        NULL,
-        LOAD_LIBRARY_SEARCH_DEFAULT_DIRS | LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR
-    );
+    *module = LoadLibraryExA(path, NULL,
+                             LOAD_LIBRARY_SEARCH_DEFAULT_DIRS |
+                                 LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR);
     if (*module == NULL)
         return OScInternal_Error_Unknown();
     return OSc_OK;
